dc1ab6fb3728fd46908b61a3d5f16549d19558fc
stuart <stuart@picea.net.au>
2017-12-01 17:37:32 +0100

    December 1st 2017

---
+++ AdventOfCode2017/AdventOfCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AdventOfCode2017
+{
+    class AdventOfCode
+    {
+        static void Main(string[] args)
+        {
+            Dec01.Run();
+
+            Console.Read();
+            Console.WriteLine("KTHNXBYE");
+        }
+
+
+    }
+}
---
+++ AdventOfCode2017/ConsoleUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public static class ConsoleUtils
+    {
+        public static void WriteColoufulBool(bool value)
+        {
+            WriteColoufulBool(value, ConsoleColor.Green, ConsoleColor.Red);
+        }
+
+        public static void WriteColoufulBool(bool value, ConsoleColor truecolour, ConsoleColor falsecolour)
+        {
+            var curColour = Console.ForegroundColor;
+            Console.ForegroundColor = truecolour;
+
+            if (!value)
+                Console.ForegroundColor = falsecolour;
+
+            Console.Write(value);
+
+            Console.ForegroundColor = curColour;
+        }
+    }
+}
